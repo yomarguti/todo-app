ad03b36b789f4ed73eec3924847d2948964ddfe7
Yomar Gutierrez Mejia <yomarguti@Yomars-MacBook-Pro.local>
2018-10-01 23:34:09 -0500

    Patch todo/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-var {mongoose} = require('./mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const {mongoose} = require('./mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 //Server for routes
 
@@ -71,6 +72,36 @@ app.delete('/todos/:id', (req, res) => {
     }).catch(e => {
         return res.status(404).send({})
     });
+})
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    var body = _.pick(req.body, ['text', 'completed'])
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send({})
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body},{new: true}).then(todo => {
+        if (!todo) {
+            return res.status(404).send({});
+        }
+
+        res.status(200).send({todo});
+    })
+    .catch(e => {
+        res.status(400).send({});
+    })
+
+
 })
 
 app.listen(port, () => {
